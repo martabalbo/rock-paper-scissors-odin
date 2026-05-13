cdd56453abde8be7c4e35eb490f7b780122dfa53
martabalbo <marta99.balbo@gmail.com>
2026-05-13 13:00:45 +0200

    Add playRound function

--- rps.js
+++ rps.js
@@ -16,3 +16,38 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = HumanChoice.toLowerCase();
+    
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("It's a draw! You both chose rock");
+        } else if (computerChoice == "paper") {
+            console.log("You lose! Paper beats rock");
+        } else if (computerChoice == "scissors") {
+            console.log("You win! Rock beats scissors");
+        };
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "paper") {
+            console.log("It's a draw! You both chose paper");
+        } else if (computerChoice == "scissors") {
+            console.log("You lose! Scissors beats paper");
+        } else if (computerChoice == "rock") {
+            console.log("You win! Paper beats rock");
+        };
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "scissors") {
+            console.log("It's a draw! You both chose scissors");
+        } else if (computerChoice == "rock") {
+            console,log("You lose! Rock beats scissors");
+        } else if (computerChoice == "paper") {
+            console,log("You win! Scissors beats paper");
+        };
+    }
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
