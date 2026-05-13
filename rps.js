ec7e95ce8e338d6ee7551b0e00563ad91e0b1875
martabalbo <marta99.balbo@gmail.com>
2026-05-13 12:03:30 +0200

    Add function getComputerChoice to generate a random choice between rock, paper and scissors

---
+++ rps.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let choice = "rock";
+    let num = Math.trunc((Math.random() * 100)) % 3;
+    if (num == 0) {
+        choice = "paper";
+    } else if (num == 1) {
+        choice = "scissors";
+    };
+    return choice;
+};
